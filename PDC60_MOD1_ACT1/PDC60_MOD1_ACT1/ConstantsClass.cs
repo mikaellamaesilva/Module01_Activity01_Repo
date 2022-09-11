1dd7db27bd6e83ed16ffabfbb1ad7ae6c44cb24d
Mikaella Mae Silva <mikaellamaecsilva@gmail.com>
2022-09-11 14:52:15 +0800

    first commit

---
+++ PDC60_MOD1_ACT1/PDC60_MOD1_ACT1/ConstantsClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PDC60_MOD1_ACT1
+{
+    public static class ConstantsClass
+    {
+        public static double Header = 30;
+        public static double Body = 14;
+        public static double Reference = 10;
+        public static double Subheader = 16;
+
+        public static readonly Color HeaderColor = Color.White;
+      
+
+    }
+}
---
+++ PDC60_MOD1_ACT1/PDC60_MOD1_ACT1/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PDC60_MOD1_ACT1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CreateCommand = new Command<Type>((Type viewType) =>
+
+            {
+                View view = (View)Activator.CreateInstance(viewType);
+                view.VerticalOptions = LayoutOptions.Center;
+                view.BackgroundColor = Color.FromHex("#faeba7");
+                view.WidthRequest = 50;
+                view.HeightRequest = 80;
+                view.Margin = new Thickness (30,0,30,10);
+
+                stackLayout.Children.Add(view);
+            });
+            BindingContext = this;
+        }
+
+        public ICommand CreateCommand { private set; get; }
+
+        private void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            var value = args.NewValue.ToString("0");
+            valueLabel.Text = String.Format("Rate how much you like Xamarin : {0}", value);
+
+        }
+
+
+    }
+    
+}
